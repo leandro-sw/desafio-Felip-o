6fa6c103d42e840a5abdff0159f02f9853916a03
davi santos <azdavileandro@gmail.com>
2025-04-07 23:56:00 -0400

    projeto finalizado

---
+++ index.js
@@ -0,0 +1,33 @@
+let nome = "davi santos"
+let nivelXp = 900
+
+if (nivelXp < 1001 ){
+    console.log ("O heroi de nome " + nome+ " está no nivel ferro")
+}
+else if (nivelXp < 2001){
+    console.log ("O heroi de nome " + nome+ " está no nivel bronze")
+}
+else if (nivelXp < 5001){
+    console.log ("O heroi de nome " + nome+ " está no nivel prata")
+}
+else if (nivelXp < 7001){
+    console.log ("O heroi de nome " + nome+ " está no nivel ouro")
+}
+else if (nivelXp < 8001){
+    console.log ("O heroi de nome " + nome+ " está no nivel platina")
+}
+else if (nivelXp < 9001){
+    console.log ("O heroi de nome " + nome+ " está no nivel Ascendente")
+}
+else if (nivelXp < 10001){
+    console.log ("O heroi de nome " + nome+ " está no nivel Imortal")
+}
+else{
+    console.log ("O heroi de nome " + nome+ " está no nivel Radiante")
+}
+
+
+
+
+    
+
